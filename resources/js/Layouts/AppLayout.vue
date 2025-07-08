c217b9b39960ec591c06b881762ee60305b0ffb2
Diogenes estevao <diogenestotal@gmail.com>
2025-07-07 22:59:34 -0300

    ajustes no dashboard

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -49,7 +49,7 @@ const logout = () => {
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboardsss
+                                    Dashboard
                                 </NavLink>
                                 <NavLink :href="route('admin.posts.index')" :active="route().current('admin.posts.*')">
                                     Post
