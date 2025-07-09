02d8d3ba58034a6dc6149b7d9cc617430e2a896c
Diogenes estevao <diogenestotal@gmail.com>
2025-07-08 21:36:50 -0300

    Trocando o nome Dashboard para development

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -49,7 +49,7 @@ const logout = () => {
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
+                                    Development
                                 </NavLink>
                                 <NavLink :href="route('admin.posts.index')" :active="route().current('admin.posts.*')">
                                     Post
